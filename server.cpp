739bb6c4127eb6d851c1f0c9142d5dc0ced7a9d1
Tian Yu <gasnus@gmail.com>
2023-04-23 09:09:51 +0200

    04 Protocol Parsing

--- server.cpp
+++ server.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +8,8 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+
+const size_t k_max_msg = 4096;
 
 static void msg(const char *msg)
 {
@@ -20,19 +23,85 @@ static void die(const char *msg)
     abort();
 }
 
-static void do_something(int connfd)
+static int32_t read_full(int fd, char *buf, size_t n)
 {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0)
+    while (n > 0)
     {
-        msg("read() error");
-        return;
+        ssize_t rv = read(fd, buf, n);
+        printf("read %ld bytes\n", rv);
+        if (rv <= 0)
+        {
+            return -1; // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
     }
-    printf("client says: %s\n", rbuf);
+    return 0;
+}
 
-    char wbuf[] = "world";
-    write(connfd, wbuf, sizeof(wbuf));
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1; // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t one_request(int connfd)
+{
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+        {
+            msg("EOF");
+        }
+        else
+        {
+            msg("read header error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4); // assume little endian
+    if (len > k_max_msg)
+    {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err)
+    {
+        msg("read request error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client syas: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
 }
 
 int main()
@@ -52,7 +121,7 @@ int main()
     addr.sin_family = AF_INET;
     addr.sin_port = ntohs(1234);
     addr.sin_addr.s_addr = ntohl(0);
-    int rv = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
     if (rv)
     {
         die("bind()");
@@ -76,7 +145,15 @@ int main()
             continue; // error
         }
 
-        do_something(connfd);
+        // only servers one client connection at once
+        while (true)
+        {
+            int32_t err = one_request(connfd);
+            if (err)
+            {
+                break;
+            }
+        }
         close(connfd);
     }
 
